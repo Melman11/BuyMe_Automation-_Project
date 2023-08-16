2224a3389dd2dbc0087c0dbecdaa6c73fd27c3b9
nivis <nivisraeli999@gmial.com>
2023-08-16 19:38:40 +0300

    Added sender_receiver_info.py

---
+++ sender_receiver_info.py
@@ -0,0 +1,12 @@
+from seleniumpagefactory.Pagefactory import PageFactory
+
+
+# This class holds all the functions for the
+class PickBusiness(PageFactory):
+    def __init__(self, driver):
+        self.driver = driver
+
+    # locators for the webelements in the sender and receiver info screen
+    locators = {
+        
+    }
