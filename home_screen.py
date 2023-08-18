7782dd2ec481619467a451cb5904057e3692ddfd
nivis <nivisraeli999@gmial.com>
2023-08-18 18:12:35 +0300

    Added Allure and some error handling

--- home_screen.py
+++ home_screen.py
@@ -1,4 +1,6 @@
 from seleniumpagefactory.Pagefactory import PageFactory
+import allure
+from allure_commons.types import AttachmentType
 
 
 # This class holds all the functions for the home screen
@@ -8,31 +10,58 @@ class HomeScreen(PageFactory):
 
     # locators for the webelements in the login
     locators = {
-        'PriceDropDown': ('CSS', "#ember1053 > div > div.selected-name"),
-        'AreaDropDown': ('CSS', "#ember1088 > div > div.selected-name"),
-        'CategoryDropDown': ('CSS', "#ember1120 > div > div.selected-name"),
-        'PriceRange': ('ID', "ember1077"),
-        'Area': ('ID', "ember1111"),
-        'Category': ('ID', "ember1172"),
-        'Search': ('ID', "ember1199")
+        'PriceDropDown': ('XPATH', '//*[@id="ember1053"]/div/div[1]/span'),
+        'AreaDropDown': ('XPATH', '//*[@id="ember1088"]/div/div[1]/span'),
+        'CategoryDropDown': ('XPATH', '//*[@id="ember1120"]/div/div[1]/span'),
+        'PriceRange': ('XPATH', '//*[@id="ember1077"]/span'),
+        'Area': ('XPATH', '//*[@id="ember1207"]'),
+        'Category': ('XPATH', '//*[@id="ember1268"]'),
+        'Search': ('XPATH', '//*[@id="ember1295"]')
 
     }
 
     # pick a price from the price dropdown
     def set_price(self):
-        self.priceDropDown.click_button()
-        self.PriceRange.click_button()
+        try:
+            self.priceDropDown.click_button()  # cliks on the dropdown for price range
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="PriceDropDownScreenshot",
+                          attachment_type=AttachmentType.PNG)
+        try:
+            self.PriceRange.click_button()  # clicks on the desired price
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="PriceRangeScreenshot",
+                          attachment_type=AttachmentType.PNG)
 
     # pick an area from the area dropdown
     def set_area(self):
-        self.AreaDropDown.click_button()
-        self.Area.click_button()
+        try:
+            self.AreaDropDown.click_button()  # cliks on the dropdown for area
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="AreaDropDownScreenshot",
+                          attachment_type=AttachmentType.PNG)
+        try:
+            self.Area.click_button()  # clicks on the desired area
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="AreaDownScreenshot",
+                          attachment_type=AttachmentType.PNG)
 
     #  pick a category from the category dropdown
     def set_category(self):
-        self.CategoryDropDown.click_button()
-        self.Category.click_button()
+        try:
+            self.CategoryDropDown.click_button()  # cliks on the dropdown for category
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="CategoryDropDownScreenshot",
+                          attachment_type=AttachmentType.PNG)
+        try:
+            self.Category.click_button()  # clicks on the desired category
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="CategoryScreenshot",
+                          attachment_type=AttachmentType.PNG)
 
     # click the search button
     def search(self):
-        self.Search.click_button()
+        try:
+            self.Search.click_button()
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="SearchScreenshot", attachment_type=AttachmentType.PNG)
--- login_page.py
+++ login_page.py
@@ -1,5 +1,6 @@
 from seleniumpagefactory.Pagefactory import PageFactory
-
+import allure
+from allure_commons.types import AttachmentType
 
 # This class holds all the functions for the login page
 class LoginPage(PageFactory):
@@ -7,16 +8,52 @@ class LoginPage(PageFactory):
         self.driver = driver
     # locators for the webelements in the login page
     locators = {
-        'loginlink': ('XPATH', "//a[@aria-label='כניסה / הרשמה'][@role='link']"),
-        'enteremail': ('ID', 'ember1888'),
-        'enterpassword': ('ID', "ember1895"),
-        'enterbuyme': ('ID', "ember1904"),
+        'LoginLink': ('XPATH', '//*[@id="ember1005"]/div/ul[1]/li[3]/a'),
+        'Register': ('XPATH','//*[@id="ember965"]/div/div[1]/div[2]/div/div[3]/div[1]/span'),
+        'Name':('XPATH','//*[@id="ember1796"]'),
+        'EnterEmail': ('XPATH', '//*[@id="ember1803"]'),
+        'EnterPassword': ('XPATH', '//*[@id="valPass"]'),
+        'ValPassword': ('XPATH', '//*[@id="ember1817"]'),
+        'AgreeToPolicy': ('XPATH', '//*[@id="ember1823"]/div/span[1]/svg/circle'),
+        'EnterBuyMe': ('XPATH', '//*[@id="ember1827"]'),
 
     }
 
-    # Set email and password and logs in to the website
+    # set email and password and logs in to the website
+    # takes screenshots and adds them to the allure
     def enter_credentials(self):
-        self.loginlink.click_button()
-        self.enteremail.set_text("wodocir726@weishu8.com")
-        self.enterpassword.set_text("Zubur123")
-        self.enterbuyme.click_button()
+        try:
+            self.LoginLink.click_button() # clicks the enter/register button
+        except :
+            allure.attach(self.driver.get_screenshot_as_png(), name="LoginLinkScreenshot", attachment_type=AttachmentType.PNG)
+        try:
+            self.Register.click_button() # clicks the register button
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="RegisterScreenshot",
+                          attachment_type=AttachmentType.PNG)
+        try:
+            self.Name.set_text("tester") # enters a name
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="NameScreenshot", attachment_type=AttachmentType.PNG)
+
+        try:
+            self.EnterEmail.set_text("wodocir727@weishu8.com") # enters an email
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="EnterEmailScreenshot", attachment_type=AttachmentType.PNG)
+        try:
+            self.EnterPassword.set_text("Tester123") # enters a password
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="EnterPasswordScreenshot", attachment_type=AttachmentType.PNG)
+        try:
+            self.ValPassword.set_text("Tester123") # validate the password
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="ValPasswordScreenshot", attachment_type=AttachmentType.PNG)
+        try:
+            self.AgreeToPolicy.click_button() # agrees to site policy
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="AgreeToPolicyScreenshot", attachment_type=AttachmentType.PNG)
+        try:
+            self.EnterBuyMe.click_button() # register to the website
+        except:
+            allure.attach(self.driver.get_screenshot_as_png(), name="EnterBuyMeScreenshot", attachment_type=AttachmentType.PNG)
+
