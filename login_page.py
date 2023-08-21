1ddeb727d256db55b1f29682dd71bae602e7cde6
nivis <nivisraeli999@gmial.com>
2023-08-21 20:30:54 +0300

    Added additional fixes

--- login_page.py
+++ login_page.py
@@ -11,14 +11,13 @@ class LoginPage(PageFactory):
     # locators for the webelements in the login page
     locators = {
         'LoginLink': ('XPATH', '//*[@id="ember1005"]/div/ul[1]/li[3]/a'),
-        'Register': ('XPATH', '//*[@id="ember965"]/div/div[1]/div[2]/div/div[3]/div[1]/span'),
+        'Register': ('XPATH', '//*[@id="ember981"]/div/div[1]/div[2]/div/div[3]/div[1]/span'),
         'Name': ('XPATH', '//*[@id="ember1796"]'),
         'EnterEmail': ('XPATH', '//*[@id="ember1803"]'),
         'EnterPassword': ('XPATH', '//*[@id="valPass"]'),
         'ValPassword': ('XPATH', '//*[@id="ember1817"]'),
         'AgreeToPolicy': ('XPATH', '//*[@id="ember1823"]/div/span[1]/svg/circle'),
         'EnterBuyMe': ('XPATH', '//*[@id="ember1827"]'),
-
 
     }
 
@@ -37,6 +36,7 @@ class LoginPage(PageFactory):
         except:
             allure.attach(self.driver.get_screenshot_as_png(), name=screenshot_name,
                           attachment_type=AttachmentType.PNG)
+
 
     def enter_credentials(self):
         self.safe_click(self.LoginLink, "LoginLinkScreenshot")  # clicks the login link
--- main_test.py
+++ main_test.py
@@ -1,9 +1,6 @@
-import time
 import pytest
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 import json
 from login_page import LoginPage
 from home_screen import HomeScreen
@@ -11,8 +8,8 @@ from pick_business import PickBusiness
 from sender_receiver_info import SenderReceiver
 
 
-@pytest.fixture
-def setUp():
+@pytest.fixture(scope="class")
+def driver_setup(request):
     # opens json file
     json_file = open('config.json', 'r')
     data = json.load(json_file)
@@ -20,49 +17,48 @@ def setUp():
     browser = data['browserType']
     if browser == 'chrome':
         driver = webdriver.Chrome(
-            service=Service("C:\\Users\\nivis\\Downloads\\chromedriver-win64\\chromedriver-win64.exe"))
+            service=Service("C:\\Users\\nivis\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe"))
     elif browser == 'firefox':
-        pass
-        # driver = webdriver.Firefox(service=Service('FIREFOXDRIVER_PATH'))
+        driver = webdriver.Firefox(
+            service=Service("C:\\Users\\nivis\\Downloads\\geckodriver-v0.33.0-win64\\geckodriver.exe"))
+    # implicit wait
+    driver.implicitly_wait(5)
+    # page load timeout
+    driver.set_page_load_timeout(8)
+    # maximize the browser window
+    driver.maximize_window()
+    # make the driver instance available for the test functions
+    request.cls.driver = driver
     # opens the webpage based on the json
     url = data['URL']
     driver.get(url)
-    # implicit wait
-    driver.implicitly_wait(10)
-    # page load timeout
-    driver.set_page_load_timeout(10)
-    yield driver
+    yield
     driver.quit()
 
 
-def test_login_page(setUp):
-    driver = setUp
-    login_test = LoginPage(driver)
-    login_test.enter_credentials()
+@pytest.mark.usefixtures("driver_setup")
+class TestYourWebsiteTests:
+    def test_login_page(self):
+        login_test = LoginPage(self.driver)
+        login_test.enter_credentials()
 
+    def test_home_screen(self):
+        home_screen = HomeScreen(self.driver)
+        home_screen.set_price()
+        home_screen.set_area()
+        home_screen.set_category()
+        home_screen.search()
 
-def test_home_screen(setUp):
-    driver = setUp
-    home_screen = HomeScreen(driver)
-    home_screen.set_price()
-    home_screen.set_area()
-    home_screen.set_category()
-    home_screen.search()
+    def test_pick_business(self):
+        site_url = self.driver.current_url
+        desired_url = "https://buyme.co.il/search?budget=3&category=419&region=13"
+        assert site_url == desired_url
+        pick_business = PickBusiness(self.driver)
+        pick_business.select_business()
+        pick_business.enter_amount()
 
-
-def test_pick_business(setUp):
-    driver = setUp
-    site_url = driver.current_url
-    desired_url = "https://buyme.co.il/search?budget=3&category=419&region=13"
-    assert site_url == desired_url
-    pick_business = PickBusiness(driver)
-    pick_business.select_business()
-    pick_business.enter_amount()
-
-
-def test_sender_receiver_info(setUp):
-    driver = setUp
-    sender_receiver_info = SenderReceiver(driver)
-    sender_receiver_info.gift_receiver()
-    sender_receiver_info.upload_picture()
-    sender_receiver_info.enter_email()
+    def test_sender_receiver_info(self):
+        sender_receiver_info = SenderReceiver(self.driver)
+        sender_receiver_info.gift_receiver()
+        sender_receiver_info.upload_picture()
+        sender_receiver_info.enter_email()
